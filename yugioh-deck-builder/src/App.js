bd010410618db58eb17522d7799f90399c723970
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-10-23 21:08:44 +0200

    Navbar created, styled and logo added

--- yugioh-deck-builder/src/App.js
+++ yugioh-deck-builder/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './Components/navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
