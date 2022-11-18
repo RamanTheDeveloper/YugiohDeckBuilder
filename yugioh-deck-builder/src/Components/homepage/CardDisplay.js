a4508bd2277ef2a05ca7f3d46c091a9ff43eb686
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-11-18 18:24:47 +0100

    Grid for cards created

--- yugioh-deck-builder/src/Components/homepage/CardDisplay.js
+++ yugioh-deck-builder/src/Components/homepage/CardDisplay.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { Grid } from 'gridjs-react'
 
 function CardDisplay() {
+
     return (
-        <Grid container spacing={2}>
-            
-        </Grid>
+        <div className='flex justify-center align-middle'>
+            <div className='box-border h-[40rem] w-[75rem] border-2'>
+                <div className='box-border h-40 w-auto border-2 flex-col'>
+                    
+                </div>
+            </div>
+
+        </div>
+
     )
 }
 
