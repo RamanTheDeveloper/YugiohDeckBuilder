beba16eb9ae2ea605c6669e7fe7773965a59eb70
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-12-11 19:16:50 +0100

    Card display and buttons added  + styled

--- yugioh-deck-builder/src/Components/homepage/CardDisplay.js
+++ yugioh-deck-builder/src/Components/homepage/CardDisplay.js
@@ -4,18 +4,26 @@ import Cards from '../../../src/JsonData/test.json'
 
 function CardDisplay() {
 
-
     return (
         <div className='flex justify-center align-middle'>
             <div className='box-border h-[40rem] w-[75rem] border-2'>
-                <div className='box-border h-40 w-auto border-2 flex-col'>
-                    <div>
+                <div className='w-max flex-col'>
+                    <div className='flex h-max w-[75rem]'>
                     {Cards.data.map(card => {
                             return(
-                                <div key={card.id}>
-                                    <h4>{card.name}</h4>
-                                    <p>{card.desc}</p>
-                                    <img src={card.card_images[0].image_url_small}/>
+                                <div key={card.id} className='flex flex-row justify-evenly align-middle h-max w-[75rem] gap-3 box-border border-2'>
+                                    <div className='flex h-40'>
+                                        <img src={card.card_images[0].image_url_small}/>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <h4><b>Name:</b> {card.name}</h4>
+                                        <p><b>Description:</b> {card.desc}</p>
+                                        <p><b>Card Type:</b> {card.type}</p>
+                                    </div>
+                                    <div className="flex flex-row justify-center align-middle gap-3 w-30 h-max">
+                                        <button className='bg-red-500 text-white font-medium h-14 w-auto rounded p-2 shadow-md hover:bg-red-800 hover-shadow-lg'>Wishlist</button>
+                                        <button className='bg-black text-white font-medium h-14 w-auto rounded p-2 shadow-md hover:bg-gray-800 hover-shadow-lg'>Ownlist</button>
+                                    </div>
                                 </div>
                             )
                         })}
