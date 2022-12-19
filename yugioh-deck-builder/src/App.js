c6dc46e68c42117033f0aef252c1255d601cb606
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-12-19 10:32:56 +0100

    Firebase initialized

--- yugioh-deck-builder/src/App.js
+++ yugioh-deck-builder/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Homepage from './Components/homepage/Homepage';
 import Navbar from './Components/navbar/Navbar';
+import { initializeApp } from 'firebase/app'
 
 function App() {
   return (
