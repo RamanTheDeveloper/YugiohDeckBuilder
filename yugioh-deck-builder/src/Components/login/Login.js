7fd33ad913170733af48f1a840f8b2d2b7b8b359
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2023-01-09 19:12:21 +0100

    Login page styled

--- yugioh-deck-builder/src/Components/login/Login.js
+++ yugioh-deck-builder/src/Components/login/Login.js
@@ -1,47 +1,101 @@
-import React, {useState} from 'react'
-import {login} from '../../Firebase/Auth'
+import React, { useState } from 'react'
+import { login } from '../../Firebase/Auth'
+
+const image = require('../images/Yugi-Joey-Kaiba.png')
 
 const Login = () => {
-	const [form,setForm] = useState({
-		email:'',
-		password:''
+	const [form, setForm] = useState({
+		email: '',
+		password: ''
 	})
-	const handleSubmit = async(e)=>{
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 		await login(form);
 
 	}
 	const InputFields = {
-		padding:'0.5rem',
-		margin:'0.8rem',
+		padding: '0.5rem',
+		margin: '0.8rem',
 		borderRadius: '4px'
 	}
 	const ButtonStyle = {
 		borderRadius: '4px',
-		padding:'0.7rem',
-		margin:'0.5rem'
+		padding: '0.7rem',
+		margin: '0.5rem'
 	}
 	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit} >
-			<label for="email">Email</label>
-			<input type="text" style={InputFields}
-				placeholder="email" id="mail"
-			onChange={(e) =>
-			setForm({...form, email: e.target.value})} />
-			<br/>
-			<label for="password">Password</label>
-			<input type="password" placeholder="Password"
-				style={InputFields}
-			onChange={(e) =>
-			setForm({...form, password: e.target.value})}/>
-			<br/>
-			<button type="submit" style={ButtonStyle}>
-				Submit
-			</button>
-			</form>
-		</div>
+		<section class="h-screen">
+			<div class="px-6 py-12 h-full w-full">
+				<div class="flex flex-col justify-center align-middle items-center flex-wrap h-full g-4 text-gray-800">
+					<div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0 w-full flex justify-center">
+						<img
+							src={image}
+							class="w-[45rem] p-4"
+							alt="Phone image"
+						/>
+					</div>
+					<div class="md:w-8/12 lg:w-5/12 lg:ml-20">
+						<form>
+							<div class="mb-6">
+								<input
+									type="text"
+									class="form-control block w-[44rem] px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+									placeholder="Email address"
+								/>
+							</div>
+
+							<div class="mb-6">
+								<input
+									type="password"
+									class="form-control block w-[44rem] px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+									placeholder="Password"
+								/>
+							</div>
+
+							<div class="flex justify-between items-center mb-6 w-[44rem]">
+								<div class="form-group form-check">
+									<input
+										type="checkbox"
+										class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-slate-800 checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+										id="exampleCheck3"
+										checked
+									/>
+									<label class="form-check-label inline-block text-gray-800" for="exampleCheck2"
+									>Remember me</label
+									>
+								</div>
+								<a
+									href="#!"
+									class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+								>Forgot password?</a
+								>
+							</div>
+
+
+							<button
+								type="submit"
+								class="inline-block px-7 py-3 bg-black text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-slate-700  hover:shadow-lg focus:bg-slate-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-900  active:shadow-lg transition duration-150 ease-in-out w-[44rem]"
+								data-mdb-ripple="true"
+								data-mdb-ripple-color="light"
+							>
+								Sign in
+							</button>
+
+							<div class="text-center lg:text-left">
+								<p class="text-sm font-semibold mt-2 pt-1 mb-0">
+									Don't have an account?
+									<a
+										href="#!"
+										class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+									>   Register</a>
+								</p>
+							</div>
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
 
 	)
 }
