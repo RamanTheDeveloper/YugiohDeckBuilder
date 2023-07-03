de3e2eadbb6677847795df2711d411c021262f5b
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2023-07-03 11:36:50 +0200

    Styling deck list page

--- yugioh-deck-builder/src/Components/decks/DeckList.js
+++ yugioh-deck-builder/src/Components/decks/DeckList.js
@@ -8,11 +8,15 @@ function DeckList() {
           <div className='font-bold flex justify-center'>
             <h1>Decks</h1>
           </div>
-          <div className='flex flex-col justify-center align-middle'>
-            <input type="text" name="Search" placeholder='Search deck...'/>
-            <a href="#">Create</a>
+          <div className='flex flex-col justify-center align-middle gap-2'>
+            <input type="text" name="Search" placeholder='Search deck...' className='border-2 p-2'/>
+            <div className='flex justify-start align-middle gap-3'>
+              <a href="#" className='box-border border-2 border-transparent w-max h-max bg-green-500 text-white py-2 px-1 rounded'>Create</a>
+              <a href="#" className='box-border border-2 border-transparent w-max h-max bg-red-500 text-white py-2 px-1 rounded'>Delete</a>
+
+            </div>
           </div>
-          <div className='box-border border-2 flex flex-col justify-center align-middle'>
+          <div className='box-border border-2 border-black flex flex-col justify-center align-middle my-4 p-2'>
             <p>Name of deck</p>
           </div>
         </div>
