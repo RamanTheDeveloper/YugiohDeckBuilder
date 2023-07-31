cca535718d48e0ac2f31ba249491f731ed47c18e
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2023-07-31 12:42:57 +0200

    Logout bug fixed when on decklist page

--- yugioh-deck-builder/src/Components/logout/Logout.js
+++ yugioh-deck-builder/src/Components/logout/Logout.js
@@ -4,12 +4,13 @@ import { auth } from '../../Firebase/firebase'
 
 function Logout() {
 
-    const logout = () => {
-        auth.signOut().then(function() {
-            console.log("Sign out successful!")
-        }).catch(function(error){
-            console.log("Error occured!")
-        })
+    const logout = async () => {
+      try{
+        await auth.signOut()
+        console.log("Sign out successful!")
+      } catch(error){
+        console.log("Error occured!", error)
+      }
     }
 
 
@@ -19,7 +20,7 @@ function Logout() {
         <li>
             <a className="m-4 text-black bg-white px-6 py-2 rounded-2xl border-white border-2 hover:border-2-white decoration-none text-base font-semibold uppercase hover:text-white hover:bg-black hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300"
             onClick={logout}>
-              Logout
+              <Link to={'/login'}>Logout</Link>
             </a>
           </li>
     </ul>
