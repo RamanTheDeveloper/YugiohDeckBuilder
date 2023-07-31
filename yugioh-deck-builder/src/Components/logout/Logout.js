397ae13182b3bb112a32aa7492c01960685004d2
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2023-07-31 10:49:53 +0200

    Logout function fixed as it was throwing error bcuz of wrong firebase export used

--- yugioh-deck-builder/src/Components/logout/Logout.js
+++ yugioh-deck-builder/src/Components/logout/Logout.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import firebase from 'firebase/compat/app'
+import { auth } from '../../Firebase/firebase'
 
 function Logout() {
 
     const logout = () => {
-        firebase.auth().signOut().then(function() {
+        auth.signOut().then(function() {
             console.log("Sign out successful!")
         }).catch(function(error){
             console.log("Error occured!")
@@ -19,7 +19,7 @@ function Logout() {
         <li>
             <a className="m-4 text-black bg-white px-6 py-2 rounded-2xl border-white border-2 hover:border-2-white decoration-none text-base font-semibold uppercase hover:text-white hover:bg-black hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300"
             onClick={logout}>
-              <Link to="/login">Logout</Link>
+              Logout
             </a>
           </li>
     </ul>
