356ca26219c615ff03e1b1a486582f5b0cc4deee
Sing6709 <ramandeep-singh25@outlook.com>
2023-08-20 15:04:59 +0300

    Added responsive hamburger menu in navbar

--- yugioh-deck-builder/src/Components/navbar/Navbar.js
+++ yugioh-deck-builder/src/Components/navbar/Navbar.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../Firebase/firebase";
 import Logout from "../logout/Logout";
-import { db } from "../../Firebase/firebase";
+import { HiMenu, HiX } from 'react-icons/hi';
 
 function Navbar() {
   const logo = require("../images/yugioh-logo.png");
 
   const [currentUser, setCurrentUser] = useState(null)
   const [isLoggedin, setIsLoggedin] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(function (user) {
@@ -37,7 +38,55 @@ function Navbar() {
           />
         </a>
       </Link>
-      <ul className="flex justify-center align-middle list-none text-center pt-6 gap-2">
+
+      {/* Hamburger Menu Icon */}
+      <div className="md:hidden">
+        {showMenu ? (
+          <HiX className="text-white text-2xl cursor-pointer" onClick={() => setShowMenu(!showMenu)} />
+        ) : (
+          <HiMenu className="text-white text-2xl cursor-pointer" onClick={() => setShowMenu(!showMenu)} />
+        )}
+      </div>
+
+      {showMenu && (
+        <div className="md:hidden fixed top-0 left-0 w-full h-screen bg-black opacity-90 z-30">
+          <ul className="flex flex-col justify-between align-middle list-none text-center pt-6 gap-5">
+            {isLoggedin && (
+              <li className="mb-4">
+                <span className="font-bold">{auth.currentUser.email}</span>
+              </li>
+            )}
+            <li>
+              <a className="m-8 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300">
+                <Link to="/deck-list">Decks</Link>
+              </a>
+            </li>
+            <li>
+              <a className="m-8 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300">
+                Wishlist
+              </a>
+            </li>
+            <li>
+              <a className="m-8 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300">
+                Ownlist
+              </a>
+            </li>
+            <li>
+              <a className="m-4 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300">
+                <Link to="/profile">Profile</Link>
+              </a>
+            </li>
+            {isLoggedin && (
+              <li>
+                <Logout />
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+
+      {/* Desktop Menu */}
+      <ul className="hidden md:flex justify-center align-middle list-none text-center pt-6 gap-2">
         <li>
           <a className="m-8 text-white decoration-none text-base font-semibold uppercase hover:text-black hover:bg-white hover:rounded-2xl hover:px-6 hover:py-2 hover:transition-all ease-in-out duration-300">
             <Link to="/deck-list">Decks</Link>
